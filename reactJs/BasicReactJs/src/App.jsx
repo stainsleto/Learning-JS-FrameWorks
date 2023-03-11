5727401537cda2e7d2b57ae68d981e3054281187
stainsleto <stainsmault@gmail.com>
2023-03-11 05:32:01 +0530

    @Stains/Setup

---
+++ reactJs/BasicReactJs/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      
+      <p>
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
